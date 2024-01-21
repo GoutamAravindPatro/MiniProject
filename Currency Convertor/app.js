72d3aa1f2ef671173ef46548de7ce0e4706a6cb1
GoutamAravindPatro-gap <145236149+GoutamAravindPatro-gap@users.noreply.github.com>
2024-01-21 15:38:26 +0530

    updated app.js

--- Currency Convertor/app.js
+++ Currency Convertor/app.js
@@ -2,6 +2,11 @@ const URL =
   "https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies";
 
   const dropdowns = document.querySelectorAll(".dropdown select");
+  const btn = document.querySelector("form button");
+  const fromCurr = document.querySelector(".from select");
+  const toCurr = document.querySelector(".to select");
+  const msg = document.querySelector(".msg");
+
 
   // Updating the dropdown values 
   for (let select of dropdowns) {
